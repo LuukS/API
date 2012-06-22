0f990c05bdb2c0669d84747874f58fce1a759fbb
LuukS <luuk.schaminee@gmail.com>
2012-06-22 15:02:00 +0200

    Voorbeeld data
    Voor gebruik van txturl parameter (in ontwikkeling)

---
+++ data/hoofdsteden.js
@@ -0,0 +1,20 @@
+featurecollection = {
+              "type": "FeatureCollection", 
+              "features": [
+                {"geometry": {
+                    "type": "GeometryCollection", 
+                    "geometries": [
+                        {
+                            "type":"Point", 
+                            "coordinates":[582378, 233571]
+                        },
+                        {
+                            "type":"Point", 
+                            "coordinates":[579910, 183023]
+                        }
+                    ]
+                }, 
+                "type": "Feature", 
+                "properties": {}}
+              ]
+           };
